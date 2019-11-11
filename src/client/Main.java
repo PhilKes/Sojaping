e2ee5a201085ea07c9e3fb8ab8bce9f1fcc3dd8f
Jan <Jan.Komposch@gmail.com>
2019-11-11 12:55:53 +0100

    Basic Controller main GUI(Messages), Added documentation

--- src/client/Main.java
+++ src/client/Main.java
@@ -11,12 +11,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("presentation/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("presentation/gui.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
--- src/common/data/Message.java
+++ src/common/data/Message.java
@@ -1,8 +1,60 @@
 package common.data;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import java.sql.Timestamp;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "_class")
 public class Message {
+    //private String sendLanguage;
+    //private String receiveLanguage;
+    private boolean translate;
+    private String text;
+    private Timestamp timestamp;
 
+
+    public Message(/*String sendLanguage, String receiveLanguage,*/ boolean translate, String text, Timestamp timestamp) {
+        /*this.sendLanguage = sendLanguage;
+        this.receiveLanguage = receiveLanguage;*/
+        this.translate = translate;
+        this.text = text;
+        this.timestamp = timestamp;
+    }
+    /*
+    public String getSendLanguage() {
+        return sendLanguage;
+    }
+
+    public void setSendLanguage(String sendLanguage) {
+        this.sendLanguage = sendLanguage;
+    }
+
+    public String getReceiveLanguage() {
+        return receiveLanguage;
+    }
+
+    public void setReceiveLanguage(String receiveLanguage) {
+        this.receiveLanguage = receiveLanguage;
+    }
+    */
+    public boolean isTranslate() {
+        return translate;
+    }
+
+    public void setTranslate(boolean translate) {
+        this.translate = translate;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
 }

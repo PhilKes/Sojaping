c3c0a431371e9486f3487dcd0b0c011b61688b8a
Jan <Jan.Komposch@gmail.com>
2019-11-11 12:57:55 +0100

    main changed loaded fxml to login.fxml(was gui.fxml)

--- src/client/Main.java
+++ src/client/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("presentation/gui.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("presentation/login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);

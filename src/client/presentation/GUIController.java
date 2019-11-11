cfa84ba534a91a74d631a5582f69e438a4f44a0e
Jan <Jan.Komposch@gmail.com>
2019-11-11 12:55:53 +0100

    Basic Controller main GUI(Messages), Added documentation

--- src/client/presentation/GUIController.java
+++ src/client/presentation/GUIController.java
@@ -1,8 +1,43 @@
 package client.presentation;
 
-import client.Client;
+
+import common.data.Message;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+import java.sql.Timestamp;
 
 public class GUIController {
+	@FXML
+	private Button btnSend;
+	@FXML
+	private Button btnMyProfile;
+	@FXML
+	private TextArea textASendText;
+	@FXML
+	private ListView listVChat;
+	@FXML
+	private CheckBox checkTranslate;
 
-	private Client client;
+	@FXML
+	private void initialize() {
+		btnSend.setOnMouseClicked(ev ->onSendClicked());
+	}
+
+	private void onSendClicked() {
+		if(!textASendText.getText().isEmpty()){
+			Message newMessage = new Message(checkTranslate.isSelected(), textASendText.getText(), new Timestamp(System.currentTimeMillis()));
+			listVChat.getItems().add(listVChat.getItems().size(),newMessage.getTimestamp().toString() +" "+ newMessage.getText());
+			textASendText.clear();
+			//Todo send Message to Server
+		}
+		else{
+
+		}
+	}
+
+
 }
--- src/common/data/Message.java
+++ src/common/data/Message.java
@@ -1,5 +1,60 @@
 package common.data;
 
-public class Message {
+import java.sql.Timestamp;
 
+public class Message {
+    //private String sendLanguage;
+    //private String receiveLanguage;
+    private boolean translate;
+    private String text;
+    private Timestamp timestamp;
+
+
+    public Message(/*String sendLanguage, String receiveLanguage,*/ boolean translate, String text, Timestamp timestamp) {
+        /*this.sendLanguage = sendLanguage;
+        this.receiveLanguage = receiveLanguage;*/
+        this.translate = translate;
+        this.text = text;
+        this.timestamp = timestamp;
+    }
+    /*
+    public String getSendLanguage() {
+        return sendLanguage;
+    }
+
+    public void setSendLanguage(String sendLanguage) {
+        this.sendLanguage = sendLanguage;
+    }
+
+    public String getReceiveLanguage() {
+        return receiveLanguage;
+    }
+
+    public void setReceiveLanguage(String receiveLanguage) {
+        this.receiveLanguage = receiveLanguage;
+    }
+    */
+    public boolean isTranslate() {
+        return translate;
+    }
+
+    public void setTranslate(boolean translate) {
+        this.translate = translate;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
 }

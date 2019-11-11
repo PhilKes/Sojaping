34bce05153b26430b543a769fc90423bd325e6fd
Irina <Irina.ungemach@gmail.com>
2019-11-11 11:26:36 +0100

    Insert new account from Client via Server to DB

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,8 +1,10 @@
 package server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import common.data.Account;
+
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -19,32 +21,44 @@ public class Server {
 
 	private ServerSocket server;
 
-	public static void main(String[] args) throws IOException {
-		new Server(443).run();
-	}
+	private DatabaseService dbService;
 
 	private Server(int port) {
 		this.port = port;
 		this.clients = new ArrayList<>();
+		this.dbService = new DatabaseService();
 	}
-
+	public static void main(String[] args) throws IOException {
+		new Server(9900).run();
+		//		new Server(443).run();
+	}
 	private void run() throws IOException {
 		String inetAddress = InetAddress.getLocalHost().getHostAddress();
 
-		server = new ServerSocket(port,50, InetAddress.getByName(inetAddress)) {
+		server = new ServerSocket(port, 50, InetAddress.getByName(inetAddress)) {
 			protected void finalize() throws IOException {
 				this.close();
 			}
 		};
 		//server.bind(new InetSocketAddress(" 141.59.135.57", 443));
-		System.out.println("Host "+ inetAddress + " port:"+ port + "is now open.");
+		System.out.println("Host " + inetAddress + " port:" + port + "is now open.");
 
 		while (true) {
 			Socket client = server.accept();
-			String nickname = (new Scanner(client.getInputStream())).nextLine();
-			System.out.println("New Client: " + nickname + " Host:" + client.getInetAddress().getHostAddress());
+			String accountAsJson = (new Scanner(client.getInputStream())).nextLine();
+			// Hier kriegen wir ein JSON von Account User + PW
 
-			User newUser = new User(client, nickname);
+			System.out.println("New Client: " + accountAsJson + " Host:" + client.getInetAddress().getHostAddress());
+
+			Account account = this.convertJsonToAccount(accountAsJson);
+
+			// check if account exist in DB
+			// if not create new
+			// else get existing
+
+			this.dbService.insert(account);
+
+			User newUser = new User(client, account.getUserName());
 
 			this.clients.add(newUser);
 			newUser.getOutStream().println("Hi  " + newUser.getNickname());
@@ -52,6 +66,28 @@ public class Server {
 			// create a new thread for newUser incoming messages handling
 			new Thread(new UserHandler(this, newUser)).start();
 		}
+	}
+
+	private void loginOrRegisterAccount() {
+
+	}
+
+	private Account convertJsonToAccount(String jsonInString) {
+		ObjectMapper mapper = new ObjectMapper();
+		Account account = null;
+		try {
+			// JSON string to Java object
+			account = mapper.readValue(jsonInString, Account.class);
+
+			// compact print
+			System.out.println("Received Json from client: " + account);
+			// pretty print
+			String prettyStaff1 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(account);
+			System.out.println(prettyStaff1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return account;
 	}
 
 	public void removeUser(User user) {

3ff1c1f123a9ad548679e2e643554e81b08df425
Jan <Jan.Komposch@gmail.com>
2019-11-14 13:16:32 +0100

    Display send messages

--- src/client/presentation/GUIController.java
+++ src/client/presentation/GUIController.java
@@ -1,12 +1,15 @@
 package client.presentation;
 
 
+import client.Client;
 import common.data.Message;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import server.Server;
 
 import java.sql.Timestamp;
 
@@ -22,22 +25,51 @@ public class GUIController {
 	@FXML
 	private CheckBox checkTranslate;
 
+	private Client client;
+
+
 	@FXML
 	private void initialize() {
-		btnSend.setOnMouseClicked(ev ->onSendClicked());
+		btnSend.setOnMouseClicked(ev -> onSendClicked());
+		textASendText.setOnKeyReleased(event -> {if(event.getCode() == KeyCode.ENTER)onSendClicked();});
+		client=Client.getInstance(Server.SERVER_HOST, Server.SERVER_PORT);
+
 	}
 
 	private void onSendClicked() {
 		if(!textASendText.getText().isEmpty()){
-			Message newMessage = new Message(checkTranslate.isSelected(), textASendText.getText(), new Timestamp(System.currentTimeMillis()));
-			listVChat.getItems().add(listVChat.getItems().size(),newMessage.getTimestamp().toString() +" "+ newMessage.getText());
+			Message newMessage = new Message(checkTranslate.isSelected(), textASendText.getText(), new Timestamp(System.currentTimeMillis()),client.getAccount(),null);
+			displayNewMessage(newMessage);
 			textASendText.clear();
-			//Todo send Message to Server
+			//Todo send Message to Server, Sender / Receiver -> Message
+			//client.sendObject(newMessage);
 		}
 		else{
 
 		}
 	}
+	private void displayNewMessage(Message Message){
+		//Todo add Sender name to output String
+		listVChat.getItems().add(listVChat.getItems().size(),Message.getTimestamp().toString().split("\\.")[0] +" "+Message.getSender().getUserName()+": "+ Message.getText());
+	}
 
+	private void onMyProfileClicked(){
+
+	}
+	private void onContactsClicked(){
+
+	}
+	private void onChatsClicked(){
+		//necessary? Chats are not saved
+	}
+
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
 
 }
--- src/common/data/Message.java
+++ src/common/data/Message.java
@@ -8,14 +8,35 @@ public class Message {
     private boolean translate;
     private String text;
     private Timestamp timestamp;
+    Account sender;
+    Account receiver;
+
+    public Account getSender() {
+        return sender;
+    }
+
+    public void setSender(Account sender) {
+        this.sender = sender;
+    }
+
+    public Account getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Account receiver) {
+        receiver = receiver;
+    }
+//Todo Sender / Receiver ?
 
 
-    public Message(/*String sendLanguage, String receiveLanguage,*/ boolean translate, String text, Timestamp timestamp) {
+    public Message(/*String sendLanguage, String receiveLanguage,*/ boolean translate, String text, Timestamp timestamp, Account sender, Account receiver) {
         /*this.sendLanguage = sendLanguage;
         this.receiveLanguage = receiveLanguage;*/
         this.translate = translate;
         this.text = text;
         this.timestamp = timestamp;
+        this.sender = sender;
+        this.receiver = receiver;
     }
     /*
     public String getSendLanguage() {

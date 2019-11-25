eca4f6e7b3893cbbc38de19f1e5385c7b3784331
Jan <Jan.Komposch@gmail.com>
2019-11-25 12:37:50 +0100

    Group Chats GUI + Chat Tabs

--- src/client/presentation/GUIController.java
+++ src/client/presentation/GUIController.java
@@ -2,6 +2,7 @@ package client.presentation;
 
 
 import client.Client;
+import common.data.Group;
 import common.data.Message;
 import common.data.Profile;
 import javafx.application.Platform;
@@ -10,15 +11,14 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import server.Server;
 
 import java.sql.Timestamp;
-import java.text.BreakIterator;
 import java.util.ArrayList;
 
 import static common.Constants.Contexts.MESSAGE_SENT;
-import static common.Constants.Contexts.USERLIST;
 
 public class GUIController extends UIController {
 	@FXML
@@ -31,17 +31,28 @@ public class GUIController extends UIController {
 	private ListView<Message> listVChat;
 	@FXML
 	private CheckBox checkTranslate;
+    @FXML
+    private CheckBox checkBroadcast;
+    @FXML
+    private TabPane tabPaneChat;
 	@FXML
 	private ListView<Profile> tabOnlineListView;
+	@FXML
+	private ListView<Group> tabGroupChatListView;
+	@FXML
+	private ListView<Profile> listVInfo;
 
 	private ObservableList<Message> messageObservableList;
 	private ObservableList<Profile> profilesObservableList;
+	private ObservableList<Group> groupsObservableList;
+	private ObservableList<Profile> participantsObservableList;
 
 	//Todo ListView does not Auto scroll to newest message
 
 	@FXML
 	private void initialize() {
 	    //Basic GUI initialize
+        tabOnlineListView.setOnMouseClicked(e -> onContactListItemClicked(e));
 		btnSend.setOnMouseClicked(ev -> onSendClicked());
 		textASendText.setOnKeyReleased(event -> {if(event.getCode() == KeyCode.ENTER)onSendClicked();});
 		client=Client.getInstance(Server.SERVER_HOST, Server.SERVER_PORT);
@@ -49,13 +60,36 @@ public class GUIController extends UIController {
 		messageObservableList = FXCollections.observableArrayList();
 		listVChat.setItems(messageObservableList);
 		listVChat.setCellFactory(messagesListView -> new ChatListViewCell());
+		tabPaneChat.getSelectionModel().selectedItemProperty().addListener(e -> displayGroupInfo());
 		//display online Profiles initialize
 		profilesObservableList = FXCollections.observableArrayList();
 		tabOnlineListView.setItems(profilesObservableList);
 		tabOnlineListView.setCellFactory(profilesListView -> new ContactListViewCell());
+		//groupchats
+		tabGroupChatListView.setOnMouseClicked(e -> onGroupClicked(e));
+		groupsObservableList = FXCollections.observableArrayList();
+		tabGroupChatListView.setItems(groupsObservableList);
+		tabGroupChatListView.setCellFactory(groupsListView -> new GroupChatListViewCell());
+		groupsObservableList.add(new Group("test", new Profile("a",0,null,null)));
+		//list participants of the selected group
+		participantsObservableList = FXCollections.observableArrayList();
+		listVInfo.setItems(participantsObservableList);
+		listVInfo.setCellFactory(profilesListView -> new ContactListViewCell());
 
 		//TODO (Next Sprint) use ListView(of ContactList).getSelectionModel().selectedItemProperty().bind() to show correct chat
 
+	}
+
+	private void displayGroupInfo() {
+		//TODO: how to get the info, that this is a group?
+		//tabPaneChat.getSelectionModel().getSelectedItem().getText();
+		// gibt Name von Tab zurück
+	}
+
+	private void onGroupClicked(MouseEvent click) {
+		if(click.isPrimaryButtonDown()){
+
+		}
 	}
 
 	private void onSendClicked() {
@@ -78,18 +112,30 @@ public class GUIController extends UIController {
 	public void displayNewMessage(Message message) {
 		messageObservableList.add(message);
 	}
-	public void displayOnlineProfiles(ArrayList<Profile> profiles){
-		profilesObservableList.clear();
-		for (Profile p:profiles) {
-			profilesObservableList.add(p);
-		}
-	}
+	public void displayOnlineProfiles(ArrayList<Profile> profiles) {
+        profilesObservableList.clear();
+        for (Profile p : profiles) {
+            profilesObservableList.add(p);
+        }
+    }
 	private void onMyProfileClicked(){
 
 	}
-	private void onContactsClicked(){
+	private void onContactListItemClicked(MouseEvent click){
+            if(click.getClickCount() == 2){
+                Profile itemSelected = tabOnlineListView.getSelectionModel().getSelectedItem();
+                Tab newTab = new Tab();
+                newTab.setText(itemSelected.getUserName());
+                for(int i = 0; i< tabPaneChat.getTabs().size(); i++){
+                    if(tabPaneChat.getTabs().get(i).getText() == newTab.getText()){
+                        return;
+                    }
+                }
+                tabPaneChat.getTabs().add(newTab);
 
+            }
 	}
+
 	private void onChatsClicked(){
 	}
 
---
+++ src/client/presentation/GroupChatListViewCell.java
@@ -0,0 +1,46 @@
+package client.presentation;
+
+import common.data.Group;
+import common.data.Profile;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
+
+public class GroupChatListViewCell extends ListCell<Group> {
+    @FXML
+    private Label labelGroupname;
+    @FXML
+    private HBox hBox;
+
+    private FXMLLoader fxmlLoader;
+
+    @Override
+    protected void updateItem(Group group, boolean empty){
+        super.updateItem(group, empty);
+        if(empty || group == null){
+            setGraphic(null);
+            setText(null);
+        }
+        else {
+            if(fxmlLoader == null){
+                fxmlLoader = new FXMLLoader(getClass().getResource("GroupChatListCell.fxml"));
+                fxmlLoader.setController(this);
+                try {
+                    fxmlLoader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            labelGroupname.setText(group.getName());
+            setGraphic(hBox);
+        }
+
+
+
+    }
+}
